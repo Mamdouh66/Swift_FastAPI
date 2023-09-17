a6e92e40f884efc3ad360829a74f8bd047e592a0
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-09-17 19:02:53 +0300

    ✅ Added HeaderView for the login page

--- ToDoList-FastAPI/Views/LoginView.swift
+++ ToDoList-FastAPI/Views/LoginView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Header
+            HeaderView()
+            
+            // Login Form
+
+            
+            // Create Account
+            
+            Spacer()
+        }
     }
 }
 
