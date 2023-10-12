edde136c48c2a8db1e4f97fd2bdb3c50ee144024
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-10-12 09:02:49 +0300

    🛠️  Added CORS middleware

--- Backend/app/app.py
+++ Backend/app/app.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from .database import engine
 from .routers import todo, user, auth
-
-from . import models
 
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(todo.router)
 app.include_router(user.router)
 app.include_router(auth.router)
